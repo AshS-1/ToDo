b829e015b95fdcdb0b910d0d5435e9fb5b3c561f
AshS-1 <108997371+AshS-1@users.noreply.github.com>
2023-09-01 12:25:52 -0700

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  OurToDo
+//
+//  Created by Ashwika Sharma on 8/31/23.
+//
+
+import UIKit
+// UITableViewDataSource
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    var items = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
+         //Do any additional setup after loading the view.
+        title = "To Do List"
+        view.addSubview(table)
+        table.dataSource = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+        table.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                // Remove the item from the data source
+                items.remove(at: indexPath.row)
+                // Update UserDefaults
+                UserDefaults.standard.setValue(items, forKey: "items")
+                // Update the table view
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
+    
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "New Item", message: "Enter new item to be done", preferredStyle: .alert)
+        alert.addTextField { field in
+            field.placeholder = "Enter item..."
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: {[weak self] (_) in
+            if let field = alert.textFields?.first {
+                if let text = field.text, !text.isEmpty {
+                    //Enter new to do list item
+                    DispatchQueue.main.async {
+                        var currentItems = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                        currentItems.append(text)
+                        UserDefaults.standard.setValue(currentItems, forKey: "items")
+                        self?.items.append(text)
+                        self?.table.reloadData()
+                    }
+                }
+            }
+        }))
+        
+        present(alert, animated: true)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+
+
+}
+
